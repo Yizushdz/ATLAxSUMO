f0df6c1a705a2b83d67c55ddfbd1cc3279ba85a4
Jesus Hernandez <jesushdz2002ch14@gmail.com>
2024-11-19 10:19:00 -0600

    added comments on auto_liRPA files

--- configs/utils.py
+++ configs/utils.py
@@ -3,6 +3,7 @@ import json
 import numpy as np
 import inspect
 import itertools
+'''handles the generation of configuration files for agent and adversarial training'''
 
 def dict_product(d):
     '''
@@ -13,6 +14,8 @@ def dict_product(d):
     Returns;
     - A list of dictionaries with every possible configuration
     '''
+    # generate all combinations of parameter values to create various configurations
+    # for agent and adversarial training
     keys = d.keys()
     vals = d.values()
     prod_values = list(itertools.product(*vals))
@@ -20,9 +23,20 @@ def dict_product(d):
     return all_dicts
 
 def iwt(start, end, interval, trials):
+    '''
+    creates a list of values from start to end with the given interval.
+    Then, repeats this list for the given number of trials.
+    '''
+    # create sequence of values that can be used to test configuarations
+    # over repeated trial with incremental parameters values
     return list(np.arange(start, end, interval))*trials
 
 def generate_configs(base_config, params):
+    '''
+    creates config .json files for all possible configurations of parameter
+    values given in the params dictionary.'''
+     # automate creation of configuration files for agent and adversarial training
+     # with diff parameter combinations
     import __main__
     if os.path.basename(__main__.__file__) != os.path.basename(inspect.stack()[1].filename):
         # Do nothing if this function is not called in main file. This allows inclusions.
@@ -30,6 +44,7 @@ def generate_configs(base_config, params):
     suffix = os.path.splitext(os.path.basename(__main__.__file__))[0]
     config_path = f"agent_configs_{suffix}/"
     agent_path = f"agents_{suffix}/"
+    # generate all combinations of parameter values to create various configurations
     all_configs = [{**base_config, **p} for p in dict_product(params)]
     if os.path.isdir(config_path) or os.path.isdir(agent_path):
         print("rm -r '{}' '{}'".format(config_path, agent_path))
